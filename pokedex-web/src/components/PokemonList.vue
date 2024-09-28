f9574557c94aaa28178592f6c2a12fdfade1f836
Gabriel Ramos <gabri8989ramos.sk9@gmail.com>
2024-09-28 12:52:33 -0300

    feat(pokedex-web/src/components/PokemonList.vue): aprimorando componente PokemonList com cores por tipo
    Atualiza o layout para incluir informações detalhadas: número do Pokédex e tipo do Pokémon na descrição.
    Introduz propriedade 'bgColor' no componente Card, permitindo que a cor de fundo mude conforme o tipo do Pokémon.

--- pokedex-web/src/components/PokemonList.vue
+++ pokedex-web/src/components/PokemonList.vue
@@ -4,10 +4,11 @@
         v-for="pokemon in pokemons"
         :key="pokemon.id"
         :title="pokemon.name"
-        :description="`Pokémon ID: ${pokemon.id}`"
+        :description="`Num: 0${pokemon.pokedexNumber} - Tipo: ${pokemon.types.join(', ')}`"
         :image="pokemon.image"
         :weight="pokemon.weight"
         :height="pokemon.height"
+        :bgColor="getColorByType(pokemon.types[0])" 
       />
     </div>
   </template>
@@ -24,6 +25,26 @@
     data() {
       return {
         pokemons: [],
+        typeColors: {
+          fire: '#FFCCBC',
+          water: '#BBDEFB',
+          grass: '#C8E6C9',
+          electric: '#FFF9C4',
+          ice: '#E1F5FE',
+          fighting: '#FFAB91',
+          poison: '#D1C4E9',
+          ground: '#FFE0B2',
+          flying: '#B3E5FC',
+          psychic: '#F8BBD0',
+          bug: '#C5E1A5',
+          rock: '#D7CCC8',
+          ghost: '#E0E0E0',
+          dragon: '#B39DDB',
+          dark: '#B0BEC5',
+          steel: '#B0BEC5',
+          fairy: '#F1BEE7',
+          normal: '#FFFFFF',
+        },
       };
     },
     async created() {
@@ -38,16 +59,21 @@
               const detailResponse = await axios.get(pokemon.url);
               return {
                 id: detailResponse.data.id,
+                pokedexNumber: detailResponse.data.id,
                 name: detailResponse.data.name,
                 image: detailResponse.data.sprites.front_default,
-                weight: (detailResponse.data.weight / 10).toFixed(1), 
-                height: (detailResponse.data.height / 10).toFixed(1), 
+                weight: (detailResponse.data.weight / 10).toFixed(1),
+                height: (detailResponse.data.height / 10).toFixed(1),
+                types: detailResponse.data.types.map(type => type.type.name),
               };
             })
           );
         } catch (error) {
           console.error('Erro ao buscar Pokémon:', error);
         }
+      },
+      getColorByType(type) {
+        return this.typeColors[type] || '#FFFFFF'; 
       },
     },
   };

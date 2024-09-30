dfdaaef4e3a638f500db9f307779a0288256b586
Gabriel Ramos <gabri8989ramos.sk9@gmail.com>
2024-09-30 20:08:40 -0300

    style(pokemon-web/src/components/SearchFilter.vue): Remoção do fundo dos filtros

--- pokedex-web/src/components/SearchFilter.vue
+++ pokedex-web/src/components/SearchFilter.vue
@@ -67,9 +67,6 @@ export default {
   flex-direction: column;
   margin-bottom: 20px;
   padding: 20px;
-  background-color: #fff;
-  border-radius: 12px;
-  box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);
 }
 
 .search-input-wrapper {
